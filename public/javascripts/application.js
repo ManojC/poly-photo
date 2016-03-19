9286dbd00e7e5ed02d806c4a0182beddaa00fb6f
Manoj Chalode <chalodem@gmail.com>
2016-03-20 01:03:59 +0530

    photo gallery is in place with few pics. Polymer component is used. :)

---
+++ public/javascripts/application.js
@@ -0,0 +1,58 @@
+(function($, db, undefined) {
+
+    var x = document.querySelector('#m-gallery')
+    x.imageData = [{
+        url: 'images/large/aditya-manwar.jpg',
+        thumb: 'images/thumbs/aditya-manwar.jpg',
+        alt: 'aditya-manwar.jpg'
+    }, {
+        url: 'images/large/akash-tambule.jpg',
+        thumb: 'images/thumbs/akash-tambule.jpg',
+        alt: 'akash-tambule.jpg'
+    }, {
+        url: 'images/large/akshay-mahajan.jpg',
+        thumb: 'images/thumbs/akshay-mahajan.jpg',
+        alt: 'akshay-mahajan.jpg'
+    }, {
+        url: 'images/large/avinash-kulkarni.jpg',
+        thumb: 'images/thumbs/avinash-kulkarni.jpg',
+        alt: 'avinash-kulkarni.jpg'
+    }, {
+        url: 'images/large/dnyaneshwar-ayya.jpg',
+        thumb: 'images/thumbs/dnyaneshwar-ayya.jpg',
+        alt: 'dnyaneshwar-ayya.jpg'
+    }, {
+        url: 'images/large/jayesh-wavhale.jpg',
+        thumb: 'images/thumbs/jayesh-wavhale.jpg',
+        alt: 'jayesh-wavhale.jpg'
+    }, {
+        url: 'images/large/mamata-chaudhari.jpg',
+        thumb: 'images/thumbs/mamata-chaudhari.jpg',
+        alt: 'mamata-chaudhari.jpg'
+    }, {
+        url: 'images/large/sachin-rokade.jpg',
+        thumb: 'images/thumbs/sachin-rokade.jpg',
+        alt: 'sachin-rokade.jpg'
+    }, {
+        url: 'images/large/sandeep-bhalerao.jpg',
+        thumb: 'images/thumbs/sandeep-bhalerao.jpg',
+        alt: 'sandeep-bhalerao.jpg'
+    }, {
+        url: 'images/large/sandeep-gaikwad.jpg',
+        thumb: 'images/thumbs/sandeep-gaikwad.jpg',
+        alt: 'sandeep-gaikwad.jpg'
+    }, {
+        url: 'images/large/suraj-kulkarni.jpg',
+        thumb: 'images/thumbs/suraj-kulkarni.jpg',
+        alt: 'suraj-kulkarni.jpg'
+    }, {
+        url: 'images/large/tejas-mahajan.jpg',
+        thumb: 'images/thumbs/tejas-mahajan.jpg',
+        alt: 'tejas-mahajan.jpg'
+    }, {
+        url: 'images/large/ujjwal-kakade.jpg',
+        thumb: 'images/thumbs/ujjwal-kakade.jpg',
+        alt: 'ujjwal-kakade.jpg'
+    }];
+
+})(jQuery, window.db = window.db || {});
